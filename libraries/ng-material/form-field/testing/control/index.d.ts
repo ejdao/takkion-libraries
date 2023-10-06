d0f2d88a2a6a4969064e1189447c663088dcc8eb
ejdao <ejdao2019@hotmail.com>
2023-10-05 22:39:06 -0500

    :fire: Archivos de testing removidos correctamente

--- libraries/ng-material/form-field/testing/control/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { ComponentHarness } from '@takkion/ng-cdk/testing';
-
-/**
- * Base class for custom form-field control harnesses. Harnesses for
- * custom controls with form-fields need to implement this interface.
- */
-export declare abstract class MatFormFieldControlHarness extends ComponentHarness {}
-
-export {};
