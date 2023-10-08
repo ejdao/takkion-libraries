32e04d81e52919571bec817e69508f33cb5d583d
ejdao <ejdao2019@hotmail.com>
2023-10-08 10:44:24 -0500

    :sparkles: BoxFormModule agregado

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import { TAK_BOX_FORM_CONFIG } from '@takkion/ng-components/box-form';
+
+TAK_BOX_FORM_CONFIG.setValue({
+  submitButton: 'ENVIAR',
+  resetButton: 'REINICIAR',
+});
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
