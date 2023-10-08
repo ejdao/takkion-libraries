2b691b83ce4efd09a9ef2c270786c1e5f8aee7bb
ejdao <ejdao2019@hotmail.com>
2023-10-07 21:37:05 -0500

    :sparkles: Base para desarrollo y testing agregada correctamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { CustomPreloadingStrategy } from './app.preloading';
+import { MaterialModule } from './layouts/material/material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserAnimationsModule],
+  imports: [
+    BrowserAnimationsModule,
+    RouterModule.forRoot([], {
+      preloadingStrategy: CustomPreloadingStrategy,
+      scrollPositionRestoration: 'enabled',
+    }),
+    AppRoutingModule,
+    MaterialModule,
+  ],
   providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent],
 })
