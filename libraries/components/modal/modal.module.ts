a733c14823f26a39fcba362aff5e600bd0a65daa
ejdao <ejdao2019@hotmail.com>
2024-04-08 13:44:33 -0500

    :bookmark: @takkion/components@1.0.0-beta.0 liberada

--- libraries/components/modal/modal.module.ts
+++ libraries/components/modal/modal.module.ts
@@ -4,19 +4,10 @@ import { MatIconModule } from '@takkion/material/icon';
 import { TakModalComponent } from './modal.component';
 import { MatDialogModule } from '@takkion/material/dialog';
 import { MatDividerModule } from '@takkion/material/divider';
-import { TakFieldsModule } from '@takkion/components/fields';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TakModalComponent],
-  imports: [
-    MatButtonModule,
-    MatDialogModule,
-    TakFieldsModule,
-    MatDividerModule,
-    FormsModule,
-    MatIconModule,
-  ],
+  imports: [MatButtonModule, MatDialogModule, MatDividerModule, MatIconModule],
   exports: [TakModalComponent],
 })
 export class TakModalModule {}
--- libraries/utilities/README.md
+++ libraries/utilities/README.md
@@ -1,11 +1,6 @@
 # Takkion Ng Utilities
 
-This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.0.
-
-## Versions
-
-`1.0.3` : First release, `either`, `groupByKey`, `lodash (cloneDeep, orderBy)` added.
-`1.0.4` : `path` added.
+This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.1.
 
 ## Peer dependencies
 
